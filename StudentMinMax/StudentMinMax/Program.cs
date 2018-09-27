8b15902a2cf3b40af9b1e2f965f7fbbd36affd67
nerdelt <nerdelt@gmail.com>
2018-09-27 00:03:57 -0500

    Student Min Max first attempt

---
+++ StudentMinMax/StudentMinMax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StudentMinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //creates new student object
+           Student student = new Student();
+           Console.WriteLine("To display the minimum and maximum quiz score of each student, please press ENTER.");
+
+           //this allows user to press enter 
+           Console.ReadLine();
+
+           //reads the text file 
+           student.GetStudentData();
+
+           //goes through text file, assigns student's name and min/max
+           student.NameMinMax();
+           Console.ReadLine();
+        }
+    }
+}
---
+++ StudentMinMax/StudentMinMax/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+
+namespace StudentMinMax
+{
+    class Student
+    {
+        // creates string array for txt file to go into
+        public List<string> Students = new List<string>();
+
+        //creates a list for all the split strings from Students
+        public List<string> SplitList = new List<string>();
+
+        //creates list for scores to go into 
+        public List<int> Scores = new List<int>();
+
+        //creates string property to be returned 
+        public string Name { get; set; }
+
+        public void GetStudentData()
+
+        {
+            string line;
+
+            //reads txt file and creates list, each line being item in list. 
+            using (StreamReader sr = new StreamReader(@"C:\Users\nerdelt\source\repos\CG-11-2\StudentGrades\StudentGrades\studentdata.txt"))
+
+            {
+                //while loop to add each line as an element in  list Student
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Students.Add(line);
+                }
+            }
+        }
+
+
+        public void NameMinMax()
+
+        {
+            // creates loop that
+            foreach (string s in Students)
+
+            {
+               //splits each string into smaller strings and adds them to new list
+                SplitList = s.Split(' ').ToList();
+
+                //assigns name
+                Name = SplitList[0];
+
+                //removes name to just have int
+                SplitList.RemoveAt(0);
+
+                //creates a loop that 
+                foreach (string n in SplitList)
+
+                {
+                    //Scores.Add(int.Parse(n)); (another way to do this)
+                    //converts strings to int and adds them to int list
+                    Scores.AddRange(SplitList.ConvertAll(int.Parse));
+                }
+
+                    //finds min/max of scores in list
+                    int min = Scores.Min();
+                    int max = Scores.Max();
+
+                    //clears list 
+                    Scores = new List<int>();
+
+                    //writes each students name and score 
+                    Console.WriteLine($"{Name}'s minimum score: {min}; maximum score: {max}.\n");
+            }
+        }
+    }
+}
